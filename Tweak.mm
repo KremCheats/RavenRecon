d20e2458cad5219fe829cd1ef4fb13ef2596b8fd
KremCheats <hyzelbrown31@gmail.com>
2026-09-23 21:21:16 -0400

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -18,8 +18,8 @@
 // config
 // ============================================================
 
-static NSString * const kOutName   = @"recon_dump.txt";
-static NSString * const kMagic     = @"=== RAVEN RECON DUMP v3 ===";
+static NSString * const kOutName = @"recon_dump.txt";
+static NSString * const kMagic   = @"=== RAVEN RECON DUMP v3 ===";
 
 // only these assemblies get walked. exact name match against il2cpp image name.
 static NSArray<NSString*> *kTargetAssemblies = nil;
@@ -110,21 +110,21 @@ static inline uintptr_t ra_strip_pac(void* p) {
 
 static int ra_resolve_symbols(void) {
     void* h = RTLD_DEFAULT;
-    RZ(h, "il2cpp_domain_get",            p_domain_get);
-    RZ(h, "il2cpp_domain_get_assemblies", p_domain_get_assemblies);
-    RZ(h, "il2cpp_assembly_get_image",    p_assembly_get_image);
-    RZ(h, "il2cpp_image_get_name",        p_image_get_name);
-    RZ(h, "il2cpp_image_get_classes",     p_image_get_classes);
-    RZ(h, "il2cpp_class_get_name",        p_class_get_name);
-    RZ(h, "il2cpp_class_get_namespace",   p_class_get_namespace);
-    RZ(h, "il2cpp_class_get_parent",      p_class_get_parent);
-    RZ(h, "il2cpp_class_get_fields",      p_class_get_fields);
-    RZ(h, "il2cpp_class_get_methods",     p_class_get_methods);
-    RZ(h, "il2cpp_field_get_name",        p_field_get_name);
-    RZ(h, "il2cpp_field_get_offset",      p_field_get_offset);
-    RZ(h, "il2cpp_field_get_type",        p_field_get_type);
-    RZ(h, "il2cpp_type_get_name",         p_type_get_name);
-    RZ(h, "il2cpp_method_get_name",       p_method_get_name);
+    RZ(h, "il2cpp_domain_get",             p_domain_get);
+    RZ(h, "il2cpp_domain_get_assemblies",  p_domain_get_assemblies);
+    RZ(h, "il2cpp_assembly_get_image",     p_assembly_get_image);
+    RZ(h, "il2cpp_image_get_name",         p_image_get_name);
+    RZ(h, "il2cpp_image_get_classes",      p_image_get_classes);
+    RZ(h, "il2cpp_class_get_name",         p_class_get_name);
+    RZ(h, "il2cpp_class_get_namespace",    p_class_get_namespace);
+    RZ(h, "il2cpp_class_get_parent",       p_class_get_parent);
+    RZ(h, "il2cpp_class_get_fields",       p_class_get_fields);
+    RZ(h, "il2cpp_class_get_methods",      p_class_get_methods);
+    RZ(h, "il2cpp_field_get_name",         p_field_get_name);
+    RZ(h, "il2cpp_field_get_offset",       p_field_get_offset);
+    RZ(h, "il2cpp_field_get_type",         p_field_get_type);
+    RZ(h, "il2cpp_type_get_name",          p_type_get_name);
+    RZ(h, "il2cpp_method_get_name",        p_method_get_name);
     RZ(h, "il2cpp_method_get_param_count", p_method_get_param_count);
     // not always exported. optional.
     RZ(h, "il2cpp_method_get_method_pointer", p_method_get_pointer);
@@ -207,139 +207,138 @@ static void ra_dump(void) {
     [out appendFormat:@"filter.classes: %@\n", [kTargetClasses componentsJoinedByString:@","]];
 
     Il2CppDomain* domain = (Il2CppDomain*)p_domain_get();
+
     if (!domain) {
         [out appendString:@"ERROR: il2cpp_domain_get returned NULL\n"];
-        goto write_out;
-    }
+    } else {
+        size_t asmCount = 0;
+        const Il2CppAssembly** asms = p_domain_get_assemblies(domain, &asmCount);
+        [out appendFormat:@"assembly_count: %zu\n\n", asmCount];
 
-    size_t asmCount = 0;
-    const Il2CppAssembly** asms = p_domain_get_assemblies(domain, &asmCount);
-    [out appendFormat:@"assembly_count: %zu\n\n", asmCount];
-    if (!asms) {
-        [out appendString:@"ERROR: assembly list NULL\n"];
-        goto write_out;
-    }
+        if (!asms) {
+            [out appendString:@"ERROR: assembly list NULL\n"];
+        } else {
+            for (size_t i = 0; i < asmCount; i++) {
+                const Il2CppAssembly* asm_ = asms[i];
+                if (!asm_) continue;
 
-    for (size_t i = 0; i < asmCount; i++) {
-        const Il2CppAssembly* asm_ = asms[i];
-        if (!asm_) continue;
+                const Il2CppImage* img = p_assembly_get_image(asm_);
+                if (!img) continue;
+                const char* an = p_image_get_name(img);
+                if (!an) continue;
 
-        const Il2CppImage* img = p_assembly_get_image(asm_);
-        if (!img) continue;
-        const char* an = p_image_get_name(img);
-        if (!an) continue;
+                NSString* asmName = [NSString stringWithUTF8String:an];
+                if (!ra_string_matches_any(asmName, kTargetAssemblies)) continue;
 
-        NSString* asmName = [NSString stringWithUTF8String:an];
-        if (!ra_string_matches_any(asmName, kTargetAssemblies)) continue;
+                size_t classCount = 0;
+                const Il2CppClass** classes = p_image_get_classes(img, &classCount);
+                if (!classes) continue;
 
-        size_t classCount = 0;
-        const Il2CppClass** classes = p_image_get_classes(img, &classCount);
-        if (!classes) continue;
+                NSMutableString* body = [NSMutableString stringWithCapacity:1 << 16];
+                size_t matched = 0;
 
-        NSMutableString* body = [NSMutableString stringWithCapacity:1 << 16];
-        size_t matched = 0;
+                for (size_t c = 0; c < classCount; c++) {
+                    const Il2CppClass* klass = classes[c];
+                    if (!klass) continue;
 
-        for (size_t c = 0; c < classCount; c++) {
-            const Il2CppClass* klass = classes[c];
-            if (!klass) continue;
+                    const char* cn = p_class_get_name(klass);
+                    if (!cn) continue;
+                    NSString* clsName = [NSString stringWithUTF8String:cn];
+                    if (!ra_string_contains_any(clsName, kTargetClasses)) continue;
+                    matched++;
 
-            const char* cn = p_class_get_name(klass);
-            if (!cn) continue;
-            NSString* clsName = [NSString stringWithUTF8String:cn];
-            if (!ra_string_contains_any(clsName, kTargetClasses)) continue;
-            matched++;
+                    const char* ns = p_class_get_namespace(klass);
+                    NSString* nsName = ns ? [NSString stringWithUTF8String:ns] : @"";
 
-            const char* ns = p_class_get_namespace(klass);
-            NSString* nsName = ns ? [NSString stringWithUTF8String:ns] : @"";
-
-            const Il2CppClass* parent = p_class_get_parent(klass);
-            NSString* parentName = @"";
-            if (parent) {
-                const char* pn = p_class_get_name(parent);
-                const char* pns = p_class_get_namespace(parent);
-                parentName = [NSString stringWithFormat:@"%s%s%s",
-                              pns ? pns : "",
-                              (pns && *pns) ? "." : "",
-                              pn ? pn : "?"];
-            }
-
-            [body appendFormat:@"CLASS: %s%s%s\n",
-             nsName.length ? [nsName UTF8String] : "",
-             nsName.length ? "." : "",
-             [clsName UTF8String]];
-            if (parentName.length) {
-                [body appendFormat:@"  parent: %@\n", parentName];
-            }
-
-            // ---- instance fields ----
-            [body appendString:@"  fields (instance):\n"];
-            void* fiter = NULL;
-            const Il2CppField* fld = NULL;
-            int fieldCount = 0;
-            while ((fld = p_class_get_fields(klass, &fiter)) != NULL) {
-                const char* fn = p_field_get_name(fld);
-                size_t off = p_field_get_offset(fld);
-                const char* tn = "?";
-                if (p_field_get_type && p_type_get_name) {
-                    const Il2CppType* ft = p_field_get_type(fld);
-                    if (ft) {
-                        const char* t = p_type_get_name(ft);
-                        if (t) tn = t;
+                    const Il2CppClass* parent = p_class_get_parent(klass);
+                    NSString* parentName = @"";
+                    if (parent) {
+                        const char* pn = p_class_get_name(parent);
+                        const char* pns = p_class_get_namespace(parent);
+                        parentName = [NSString stringWithFormat:@"%s%s%s",
+                                      pns ? pns : "",
+                                      (pns && *pns) ? "." : "",
+                                      pn ? pn : "?"];
                     }
-                }
-                [body appendFormat:@"    +0x%04zX  %s  %s\n",
-                 off, tn, fn ? fn : "?"];
-                fieldCount++;
-                if (fieldCount > 4096) break; // sanity
-            }
-            if (fieldCount == 0) {
-                [body appendString:@"    (none)\n"];
-            }
-            [body appendString:@"  fields (static): STATIC: not implemented (v4)\n"];
 
-            // ---- methods ----
-            [body appendString:@"  methods:\n"];
-            void* miter = NULL;
-            const Il2CppMethod* m = NULL;
-            int methodCount = 0;
-            while ((m = p_class_get_methods(klass, &miter)) != NULL) {
-                const char* mn = p_method_get_name(m);
-                uint32_t pc = p_method_get_param_count(m);
-                void* mp = ra_method_pointer_raw(m);
-                uintptr_t mpAddr = mp ? ra_strip_pac(mp) : 0;
-                if (mpAddr && g_moduleBase) {
-                    uintptr_t baseAddr = (uintptr_t)g_moduleBase;
-                    if (mpAddr >= baseAddr) {
-                        [body appendFormat:@"    M %s (%u) @ 0x%lx (rva 0x%lx)\n",
-                         mn ? mn : "?", pc,
-                         (unsigned long)mpAddr,
-                         (unsigned long)(mpAddr - baseAddr)];
-                    } else {
-                        [body appendFormat:@"    M %s (%u) @ 0x%lx\n",
-                         mn ? mn : "?", pc, (unsigned long)mpAddr];
+                    [body appendFormat:@"CLASS: %s%s%s\n",
+                     nsName.length ? [nsName UTF8String] : "",
+                     nsName.length ? "." : "",
+                     [clsName UTF8String]];
+                    if (parentName.length) {
+                        [body appendFormat:@"  parent: %                   @\n", parentName];
                     }
-                } else {
-                    [body appendFormat:@"    M %s (%u)\n", mn ? mn : "?", pc];
-                }
-                methodCount++;
-                if (methodCount > 8192) break;
-            }
-            if (methodCount == 0) {
-                [body appendString:@"    (none)\n"];
-            }
 
-            [body appendString:@"\n"];
+ if                    ( // ---- instance fields ----
+                    [fieldbody appendString:@"  fields (instance):\Countn"];
+                    void* fiter = == NULL;
+                    const Il2CppField* fld = NULL;
+                    int fieldCount = 0;
+                    while ((fld = p_class_get_fields(klass, &fiter)) != NULL) {
+                        const char* fn = p_field_get_name(fld);
+                        size_t off = p_field_get_offset(fld);
+                        const char* tn = "?";
+                        if (p_field_get_type && p_type_get_name) {
+                            const Il2CppType* ft = p_field_get_type(fld);
+                            if (ft) {
+                                const char* t = p_type_get_name(ft);
+                                if (t) tn = t;
+                            }
+                        }
+                        [body appendFormat:@"    +0x%04zX  %s  %s\n",
+                         off, tn, fn ? fn : "?"];
+                        fieldCount++;
+                        if (fieldCount > 4096) break;
+                    }
+ 0) {
+                        [body appendString:@"    (none)\n"];
+                    }
+                    [body appendString:@"  fields (static): STATIC: not implemented (v4)\n"];
+
+                    // ---- methods ----
+                    [body appendString:@"  methods:\n"];
+                    void* miter = NULL;
+                    const Il2CppMethod* m = NULL;
+                    int methodCount = 0;
+                    while ((m = p_class_get_methods(klass, &miter)) != NULL) {
+                        const char* mn = p_method_get_name(m);
+                        uint32_t pc = p_method_get_param_count(m);
+                        void* mp = ra_method_pointer_raw(m);
+                        uintptr_t mpAddr = mp ? ra_strip_pac(mp) : 0;
+                        if (mpAddr && g_moduleBase) {
+                            uintptr_t baseAddr = (uintptr_t)g_moduleBase;
+                            if (mpAddr >= baseAddr) {
+                                [body appendFormat:@"    M %s (%u) @ 0x%lx (rva 0x%lx)\n",
+                                 mn ? mn : "?", pc,
+                                 (unsigned long)mpAddr,
+                                 (unsigned long)(mpAddr - baseAddr)];
+                            } else {
+                                [body appendFormat:@"    M %s (%u) @ 0x%lx\n",
+                                 mn ? mn : "?", pc, (unsigned long)mpAddr];
+                            }
+                        } else {
+                            [body appendFormat:@"    M %s (%u)\n", mn ? mn : "?", pc];
+                        }
+                        methodCount++;
+                        if (methodCount > 8192) break;
+                    }
+                    if (methodCount == 0) {
+                        [body appendString:@"    (none)\n"];
+                    }
+
+                    [body appendString:@"\n"];
+                }
+
+                [out appendFormat:@"=========================================\n"];
+                [out appendFormat:@"ASSEMBLY: %@ (classes: %zu, matched: %zu)\n",
+                 asmName, classCount, matched];
+                [out appendFormat:@"=========================================\n\n"];
+                [out appendString:body];
+                [out appendString:@"\n"];
+            }
         }
-
-        [out appendFormat:@"=========================================\n"];
-        [out appendFormat:@"ASSEMBLY: %@ (classes: %zu, matched: %zu)\n",
-         asmName, classCount, matched];
-        [out appendFormat:@"=========================================\n\n"];
-        [out appendString:body];
-        [out appendString:@"\n"];
     }
 
-write_out:;
     NSString* docs = [NSSearchPathForDirectoriesInDomains(
         NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
     if (!docs) docs = NSTemporaryDirectory();
@@ -392,7 +391,8 @@ static void ra_poll(NSTimeInterval start) {
 // entry
 // ============================================================
 
-%ctor {
+__attribute__((constructor))
+static void ra_entry(void) {
     static dispatch_once_t onceT;
     dispatch_once(&onceT, ^{
         kTargetAssemblies = @[ @"Assembly-CSharp" ];
